5d41a491121ff59938c1d6cef3c955246f738c4d
Surajit Rana <surajit1602@gmail.com>
2025-04-11 00:16:27 +0530

    feat[templates]: added preset templates

--- my-app/webpack.config.js
+++ my-app/webpack.config.js
@@ -16,9 +16,7 @@ module.exports = {
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                }
+                use: 'babel-loader',
             },
             {
                 test: /\\.css$/,
---
+++ template/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <h1>🚀 Hello from Ignite React App!</h1>
+    );
+};
+
+export default App;
